001b69234f62b7d72eefb588bb514631f732c5a8
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-14 17:26:58 +0200

    SignInForm component.

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -4,12 +4,14 @@ import { ConnectedRouter } from 'react-router-redux'
 import history from '../history'
 import App from './App'
 import SignUpForm from './SignUpForm'
+import SignInForm from './SignInForm'
 
 const Routes = () => (
   <ConnectedRouter history={history}>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/sign-up" component={SignUpForm} />
+      <Route exact path="/sign-in" component={SignInForm} />
     </Switch>
   </ConnectedRouter>
 )
