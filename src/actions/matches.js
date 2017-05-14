eeba825b546faae683c55772b4d9301e0efdfab1
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-14 19:05:33 +0200

    Matches synchronization.

--- src/actions/matches.js
+++ src/actions/matches.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import { SET_MATCHES } from '../actionTypes'
 
 export const createMatch = name => (dispatch, getState) => {
   firebase.database().ref('matches').push({
@@ -7,4 +8,27 @@ export const createMatch = name => (dispatch, getState) => {
   }).then(match => {
     console.log(match)
   })
+}
+
+export const setMatches = matches => ({
+  type: SET_MATCHES,
+  matches
+})
+
+export const watchMatches = () => dispatch => {
+  firebase.database().ref('matches').on('value', snapshot => {
+    const matchesObject = snapshot.val()
+
+    const matches = Object.keys(matchesObject).map(matchId => {
+      const match = matchesObject[matchId]
+
+      return { ...match, id: matchId }
+    })
+
+    dispatch(setMatches(matches))
+  })
+}
+
+export const unwatchMatches = () => () => {
+  firebase.database().ref('matches').off()
 }
