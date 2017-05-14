26ac537ad12f2c356a4ceb260e57e399e78e2b96
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-14 17:54:38 +0200

    signInWithGoogle action.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,6 +1,20 @@
+import firebase from 'firebase'
+import { push } from 'react-router-redux'
 import { SET_CURRENT_USER } from '../actionTypes'
 
 export const setCurrentUser = user => ({
   type: SET_CURRENT_USER,
   user
 })
+
+export const signInWithGoogle = () => dispatch => {
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+  googleAuthProvider.addScope('email')
+
+  firebase
+    .auth()
+    .signInWithRedirect(googleAuthProvider)
+    .then(() => {
+      dispatch(push('/'))
+    })
+}
