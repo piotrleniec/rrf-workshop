0a444da3c50b8f122d3b898c99ce4e17aad77b42
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-14 17:26:58 +0200

    SignInForm component.

---
+++ src/components/SignInForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { reduxForm, Field, change, reset } from 'redux-form'
+import firebase from 'firebase'
+import { push } from 'react-router-redux'
+
+const SignInForm = props => (
+  <form onSubmit={props.handleSubmit}>
+    <label>
+      Email:
+      <Field name="email" component="input" type="email" />
+    </label>
+
+    <br />
+
+    <label>
+      Password:
+      <Field name="password" component="input" type="password" />
+    </label>
+
+    <br />
+
+    <button>Sign up</button>
+  </form>
+)
+
+export default reduxForm({
+  form: 'signInForm',
+  onSubmit: (values, dispatch) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(values.email, values.password)
+      .then(() => {
+        dispatch(reset('signInForm'))
+        dispatch(push('/'))
+      })
+      .catch(() => {
+        dispatch(change('signInForm', 'password', ''))
+      })
+  }
+})(SignInForm)
