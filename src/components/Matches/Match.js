0571c427709debbe0efd1d3cae2931cad770e5f5
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-15 19:55:33 +0200

    Integrated match joining.

--- src/components/Matches/Match.js
+++ src/components/Matches/Match.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { joinMatch } from '../../actions/matches'
 
 const Match = props => (
   <div>
-    <Link to={`/matches/${props.match.id}`}>
-      <h1>{props.match.name}</h1>
-    </Link>
-    <p>Owner: {props.match.owner.email}</p>
+    <h1>{props.matchName}</h1>
+    <p>Owner: {props.ownerEmail}</p>
+    <p>Opponent: {props.opponentEmail}</p>
+
+    <Link to={`/matches/${props.matchId}`}>Show</Link>
+    &nbsp;|&nbsp;
+    {props.canJoin && <a href="#" onClick={props.joinMatch}>Join</a>}
   </div>
 )
 
-const mapStateToProps = (state, ownProps) => ({
-  match: state.matches.find(match => match.id === ownProps.matchId)
+const mapStateToProps = (state, ownProps) => {
+  const match = state.matches.find(match => match.id === ownProps.matchId)
+  const { owner, opponent } = match
+
+  return {
+    matchName: match.name,
+    ownerEmail: owner.email,
+    opponentEmail: (opponent || {}).email || '-',
+    canJoin: !opponent && owner.id !== state.currentUser.id
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  joinMatch: () => { dispatch(joinMatch(ownProps.matchId)) }
 })
 
-export default connect(mapStateToProps)(Match)
+export default connect(mapStateToProps, mapDispatchToProps)(Match)
