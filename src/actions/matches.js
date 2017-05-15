6032dc2a25b1cff64f4a2f875ef4cbc059a9d804
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-15 19:14:12 +0200

    Removed redundant console.log.

--- src/actions/matches.js
+++ src/actions/matches.js
@@ -5,8 +5,6 @@ export const createMatch = name => (dispatch, getState) => {
   firebase.database().ref('matches').push({
     name,
     owner: getState().currentUser
-  }).then(match => {
-    console.log(match)
   })
 }
 
