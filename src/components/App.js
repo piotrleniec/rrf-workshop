04f9d1adcc02082148680dea6930cc4e7b13e646
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-21 13:23:06 +0200

    CreateMessageForm in App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import SignOutButton from './SignOutButton'
 import Users from './Users'
+import CreateMessageForm from './CreateMessageForm'
 
 const App = () => (
   <div>
     <Users />
+    <br />
+    <CreateMessageForm />
+    <br />
     <SignOutButton />
   </div>
 )
