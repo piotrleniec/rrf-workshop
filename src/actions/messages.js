c12ad7651e74b38744b8e810b65bda2ffea993b1
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-21 14:15:50 +0200

    Reducer and actions for messages.

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import { SET_MESSAGES } from '../actionTypes'
 
 export const createMessage = text => (dispatch, getState) => {
   const newMessageRef = firebase.database().ref('messages').push()
@@ -9,4 +10,26 @@ export const createMessage = text => (dispatch, getState) => {
     user: getState().currentUser,
     createdAt: new Date().getTime()
   })
+}
+
+const setMessages = messages => ({
+  type: SET_MESSAGES,
+  messages
+})
+
+export const watchMessages = () => (dispatch, getState) => {
+  firebase
+    .database()
+    .ref('messages')
+    .orderByChild('createdAt')
+    .on('value', snapshot => {
+      const messagesObject = snapshot.val() || {}
+      const messages = Object.values(messagesObject)
+
+      dispatch(setMessages(messages))
+    })
+}
+
+export const unwatchMessages = () => () => {
+  firebase.database().ref('messages').off()
 }
