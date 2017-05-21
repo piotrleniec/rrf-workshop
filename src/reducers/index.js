38d7ed4f2c8d801bba58a84dfc33a7bb21117995
Piotr Leniec <leniec.piotr@gmail.com>
2017-05-21 14:15:50 +0200

    Reducer and actions for messages.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { routerReducer } from 'react-router-redux'
 import { reducer as formReducer } from 'redux-form'
 import currentUser from './currentUser'
 import users from './users'
+import messages from './messages'
 
 export default combineReducers({
   router: routerReducer,
   form: formReducer,
   currentUser,
-  users
+  users,
+  messages
 })
